78efcc041c7be234e7b598bf73cebc48a593c471
alexpouget <pouget.al@gmail.com>
2016-12-04 12:59:06 -0500

    Update Command.java

--- src/Command.java
+++ src/Command.java
@@ -1,24 +1,58 @@
+import java.io.Serializable;
+
 /**
  * Created by hugopiso on 03/12/2016.
  */
-public class Command {
+public class Command implements Serializable{
 
-    private String action;
-    private String element;
+        public Command(String command){
+            switch (command){
+                case "switch on bedroom":
+                    action = "switch on";
+                    element = "bedroom";
+                    break;
+                case "switch off bedroom":
+                    action = "switch off";
+                    element = "bedroom";
+                    break;
+                case "switch on living room":
+                    action = "switch on";
+                    element = "living room";
+                    break;
+                case "switch off living room":
+                    action = "switch off";
+                    element = "living room";
+                    break;
+                default:
+                    break;
+            }
+        }
 
-    public String getAction() {
-        return action;
+    public Command() {
     }
 
-    public void setAction(String action) {
+    public Command(String action, String element) {
         this.action = action;
-    }
-
-    public String getElement() {
-        return element;
-    }
-
-    public void setElement(String element) {
         this.element = element;
     }
+
+    private String action;
+        private String element;
+
+        public String getAction() {
+            return action;
+        }
+
+        public void setAction(String action) {
+            this.action = action;
+        }
+
+        public String getElement() {
+            return element;
+        }
+
+        public void setElement(String element) {
+            this.element = element;
+        }
+
 }
