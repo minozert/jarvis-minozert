1595f2022b30bfa4b257219a63268e3b0fed2735
minozert <64554466+minozert@users.noreply.github.com>
2020-05-16 00:44:00 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,23 +13,46 @@ public class Main {
         controlGpio.order();
 
 
-//        // Just some test waiting to get real commands via socket
-//        com.example.alex.jarvis.Command c1 = new com.example.alex.jarvis.Command("switch on bedroom");
-//        controlGpio.control(c1);
-//
-//        Thread.sleep(5000);
-//
-//        com.example.alex.jarvis.Command c2 = new com.example.alex.jarvis.Command();
-//        c2.setAction("switch off bedroom");
-//        controlGpio.control(c2);
-//
-//        com.example.alex.jarvis.Command c3 = new com.example.alex.jarvis.Command("switch on living room");
-//        controlGpio.control(c3);
-//
-//        Thread.sleep(2000);
-//
-//        com.example.alex.jarvis.Command c4 = new com.example.alex.jarvis.Command("switch off living room");
-//        controlGpio.control(c4);
+        // Just some test waiting to get real commands via socket
+        com.example.alex.jarvis.Command c1 = new com.example.alex.jarvis.Command("lantai satu");
+        c1.setAction("lantai satu");
+        controlGpio.control(c1);
+        Thread.sleep(300);
+        
+        com.example.alex.jarvis.Command c2 = new com.example.alex.jarvis.Command("lantai dua");
+        c2.setAction("lantai dua");
+        controlGpio.control(c2);
+        Thread.sleep(300);
+        
+        com.example.alex.jarvis.Command c3 = new com.example.alex.jarvis.Command("lantai tiga");
+        c3.setAction("lantai tiga");
+        controlGpio.control(c3);
+        Thread.sleep(300);
+        
+        com.example.alex.jarvis.Command c4 = new com.example.alex.jarvis.Command("lantai empat");
+        c4.setAction("lantai empat");
+        controlGpio.control(c4);
+        Thread.sleep(300);
+        
+        com.example.alex.jarvis.Command c5 = new com.example.alex.jarvis.Command("lantai lima");
+        c5.setAction("lantai lima");
+        controlGpio.control(c5);
+        Thread.sleep(300);
+        
+        com.example.alex.jarvis.Command c6 = new com.example.alex.jarvis.Command("lantai enam");
+        c6.setAction("lantai enam");
+        controlGpio.control(c6);
+        Thread.sleep(300);
+
+        com.example.alex.jarvis.Command c7 = new com.example.alex.jarvis.Command("buka pintu");
+        c7.setAction("buka pintu");
+        controlGpio.control(c7);
+        Thread.sleep(300);
+        
+        com.example.alex.jarvis.Command c8 = new com.example.alex.jarvis.Command("tutup pintu");
+        c8.setAction("tutup pintu");
+        controlGpio.control(c8);
+        Thread.sleep(300);
 
     }
 }
